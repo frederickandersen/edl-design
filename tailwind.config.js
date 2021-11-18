b2292080cb24550c0b7edaca0f45125f699e423c
Frederick Andersen <info@fredrixdesign.com>
2021-11-18 23:09:43 +0100

    2.0
    Updated with a shit ton of content. Probably needs a few fixes, but leeeeets go.

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,10 +36,19 @@ module.exports = {
         },
         extend: {
             backgroundImage: {
-                'cta': "url('/media/edl-cta.jpg')",
+                'blue-gradient': "url('/media/po-cover-bg.png')",
+                'purple-gradient': "url('/media/srm-cover-bg.png')",
+                'orange-gradient': "url('/media/bor-cover-bg.png')",
+                'green-gradient': "url('/media/oz-cover-bg.png')",
+                'radial-yellow': "radial-gradient(41.33% 41.33% at 50% 50%, #ffc738 0%, rgba(248,232,225,0) 100%)",
+                'radial-heaven': "radial-gradient(41.33% 41.33% at 50% 50%, rgba(252,164,69,1) 0%, rgba(148,187,233,0) 100%)",
+                'radial-green': "radial-gradient(41.33% 41.33% at 50% 50%, #D3DABA 0%, rgba(181,252,70,0) 100%)",
             },
             padding: {
                 '26': '6.5rem'
+            },
+            fontSize: {
+                '4.5xl': '2.75rem',
             },
             keyframes: {
                 marquee: {
@@ -58,6 +67,13 @@ module.exports = {
                 '208': '52rem',
                 '256': '64rem',
             },
+            zIndex: {
+                '-1': '-1',
+            },
+            gridTemplateColumns: {
+                'section': 'fit-content(35%) 1fr',
+                'contact': 'auto 1fr',
+            },
         },
     },
     variants: {
@@ -73,11 +89,12 @@ module.exports = {
             addComponents({
                 '.container': {
                     maxWidth: '100%',
+                    
                     '@screen sm': {
                         maxWidth: '96%',
                     },
                     '@screen 2xl': {
-                        maxWidth: '1536px',
+                        maxWidth: '88%',
                     },
                 }
             })
